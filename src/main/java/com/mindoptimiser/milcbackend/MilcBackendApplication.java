21cad4ad7f1363657b0c36bfb71d3f4322fbba1c
Chaoran-Li <lichaoran520@gmail.com>
2021-09-30 16:59:05 +1000

    set up springboot

---
+++ src/main/java/com/mindoptimiser/milcbackend/MilcBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mindoptimiser.milcbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MilcBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MilcBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mindoptimiser/milcbackend/MilcBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mindoptimiser.milcbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MilcBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
